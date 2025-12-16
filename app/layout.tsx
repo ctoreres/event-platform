c5e3669c47706ec4f999c3f1cdb92c56cf16ea84
Christan Toreres <torereschristan@gmail.com>
2025-12-16 23:01:55 +0800

    refactor: update lihgtray style

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,21 +28,23 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
       >
-      <div style={{ width: '100%', height: '600px', position: 'relative' }}>
-        <LightRays
-          raysOrigin="top-center"
-          raysColor="#00ffff"
-          raysSpeed={1.5}
-          lightSpread={0.8}
-          rayLength={1.2}
-          followMouse={true}
-          mouseInfluence={0.1}
-          noiseAmount={0.1}
-          distortion={0.05}
-          className="custom-rays"
-        />
-      </div>
-        {children}
+        <div className="absolute inset-0 z-[-1] min-h-screen">
+          <LightRays
+            raysOrigin="top-center"
+            raysColor="#5dfeca"
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.02}
+            noiseAmount={0.0}
+            distortion={0.01}
+            className="custom-rays"
+          />
+        </div>
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
