f038d52c60b3a07d707b112f4d6a279abca353cb
Christan Toreres <torereschristan@gmail.com>
2025-12-16 23:04:31 +0800

    feat: update title

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
-  return (<>
-  </>);
+const page = () => {
+  return (
+    <>
+      <h1 className='text-center'>The Hub for Every Dev <br />Event You Can't Miss</h1>
+      <p className='text-center mt-5'>Hackaton, Meetups, and Conferences, Al in One Place</p>
+    </>
+  )
 }
+
+export default page
